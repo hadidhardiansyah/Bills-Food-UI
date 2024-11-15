ca93339c39217e2dce26c787caca725e87086fe3
Hadid Hardiansyah Saputra <hadidhardiansyah@gmail.com>
2024-11-15 23:24:20 +0700

    Add restaurant card at Homapage

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./Home.css";
 import MultiItemCarousel from './MultiItemCarousel';
+import RestaurantCard from '../Restaurant/RestaurantCard';
 
+const restaurants = [1, 2, 3, 4, 5]
 const Home = () => {
   return (
-    <div className=''>
+    <div className='pb-10'>
         <section className='banner -z-50 relative flex flex-col justify-center items-center'>
             <div className='w-[50vw] z-10 text-center'>
                 <p className='text-2xl lg:text-6xl font-bold z-10 py-5'>
@@ -28,6 +30,14 @@ const Home = () => {
                 Top Meals
             </p>
             <MultiItemCarousel/>
+        </section>
+        <section className='px-5 lg:px-20 pt-10'>
+            <h1 className='text-2xl font-semibold text-gray-400 pb-8'>
+                Order From Our Handpicked Favorites
+            </h1>
+            <div className='flex flex-wrap items-center justify-around gap-5'>
+                {restaurants.map((item) => <RestaurantCard/>)}
+            </div>
         </section>
     </div>
   )
