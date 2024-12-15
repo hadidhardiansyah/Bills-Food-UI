d6f4b5efdc622ae473e0aad1f4969ccc4d8a88c4
Hadid Hardiansyah Saputra <hadidhardiansyah@gmail.com>
2024-12-15 14:27:34 +0700

    Add navigate to homepage

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -23,7 +23,7 @@ export const Navbar = () => {
   return (
     <Box className="px-5 sticky top-0 z-50 py-[.8rem] bg-[#E91E63] lg:px-20 flex justify-between">
       <div className="lg:mr-10 cursor-pointer flex items-center space-x-4">
-        <li className="logo font-semibold text-gray-300 text-2xl">
+        <li onClick={() => navigate('/')} className="logo font-semibold text-gray-300 text-2xl">
           Bills Food
         </li>
       </div>
